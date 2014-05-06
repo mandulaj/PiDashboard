555d270ceeb7965363e6a45a2e9d2c68d1673cae
Jakub Mandula <jakub.aludnam@gmail.com>
2014-05-07 00:27:55 +0200

    add self init function to Raspberry Pi

--- src/js/script.js
+++ src/js/script.js
@@ -9,8 +9,19 @@ function RaspberryPi(model)
 
     this.traqball = new Traqball({
         stage: "stage",
-        //axis: [0.5,1,0,0.25],
+        axis: [0.5,1,0,0.25],
         prespective: 1000
+    });
+    
+    this.initSelf()
+}
+
+RaspberryPi.prototype.initSelf = function()
+{
+    var thisObj = this;
+    
+    $("#default_button").click(function(){
+        thisObj.defaultPosition()
     });
 }
 
@@ -18,8 +29,8 @@ RaspberryPi.prototype.defaultPosition = function()
 {
     this.traqball.disable()
     this.model.addClass( "picontainer_mover" );
-    this.model.css( "-webkit-transform", "translateZ( 111px ) rotateX(70deg) rotateY(0deg) rotateZ(40deg)");
-    this.model.css( "transform", "translateZ( 111px ) rotateX(70deg) rotateY(0deg) rotateZ(40deg)" );
+    this.model.css( "-webkit-transform", "rotateX(58deg) rotateY(0deg) rotateZ(45deg)");
+    this.model.css( "transform", "rotateX(58deg) rotateY(0deg) rotateZ(45deg)" );
     var thisObj = this;
     setTimeout(function(){
         thisObj.model.removeClass( "picontainer_mover" );
