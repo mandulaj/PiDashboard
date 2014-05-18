48a41e08adcf07e76dd60c21406148b7b8e2bbfb
Jakub Mandula <jakub.aludnam@gmail.com>
2014-05-18 21:59:37 +0200

    abstract widgets

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -1,3 +1,17 @@
+window.pidash = {
+
+    widgets: [
+        {
+            id: "#wg-cpuInfo",
+            title: "CPU info!!!!",
+            description: "<p>Get general info about your Pi's central processor.</p>",
+        }   
+    ],
+    widgetsDown: false,
+    wObjects:[]
+}
+
+
 $(document).ready(function() {
 
     $("#widgets").owlCarousel({
@@ -14,10 +28,44 @@ $(document).ready(function() {
         if(w.height())
         {
             w.css("height", "0px");
+            window.pidash.widgetsDown = false;
         }
         else
         {
+            window.pidash.widgetsDown = true;
             w.css("height", "215px");
         }
     })
+    
+    for (var i = 0; i < window.pidash.widgets.length; i++)
+    {
+        window.pidash.wObjects.push(new Widget({
+            id: window.pidash.widgets[i].id,
+            title: window.pidash.widgets[i].title,
+            description: window.pidash.widgets[i].description
+        }));
+        
+        window.pidash.wObjects[i].render();
+    }
+    
 });
+
+
+function Widget(specs)
+{
+    var self = this;
+    this.element = $(specs.id);
+    this.id = specs.id; 
+    this.title = specs.title;
+    this.description = specs.description;
+    
+    this.element.find(".widget-top").mousedown(function(){
+        self.element.parent().draggable()
+    })
+}
+
+Widget.prototype.render = function()
+{
+    this.element.find(".widget-name").html(this.title)
+    this.element.find(".widget-content").html(this.description)
+}
