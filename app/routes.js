53105efe849e9429747d21f52a0a26afaf000296
Jakub Mandula <jakub.aludnam@gmail.com>
2014-05-18 20:41:48 +0000

    add routes authentication method

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,23 @@
 module.exports = function(app, passport, config)
 {
     app.get("/", function(req, res){
-        res.render("index.ejs");
-    })    
+        res.render("login.ejs");
+    });
+    
+    app.get("/rpi", isAuthenticated, function(req, res){
+        res.render("index.ejs")
+    });
+    
+    function isAuthenticated(req, res, next)
+    {
+        
+        if (true)
+        {
+            next();
+        }
+        else
+        {
+            res.redirect("/")
+        }
+    }
 }
