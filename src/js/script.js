7fc5ff8183a7fadd5544e912e2da35c3e6dfd1d5
Jakub Mandula <jakub.aludnam@gmail.com>
2014-09-13 11:00:29 +0200

    improve proc list

--- src/js/script.js
+++ src/js/script.js
@@ -131,12 +131,28 @@ RaspberryPi.prototype.update = function()
 
 // Object representing the List of processes
 function ProcessTable( id, rpi) {
+    var self = this;
     this.table = $(id);
     this.list = $(id+ " > tbody");
     this.rpi = rpi;
 
+    this.searchBox = $("#procSearch");
+
+    // Update when user stops typing for 0.5s
+    var typingBreak;
+    this.searchBox.keyup(function(){
+        clearTimeout(typingBreak);
+        typingBreak = setTimeout(function(){
+            self.renderProcList();
+        },500)
+    });
+    this.searchBox.keydown(function(){
+        clearTimeout(typingBreak);
+    })
+
     this.sort = 1; // id of the element we are sorting by
     this.reverse = false; // are we sorting in reverse?
+    this.total;
 }
 
 ProcessTable.prototype.renderProcList = function()
@@ -156,46 +172,48 @@ ProcessTable.prototype.renderProcList = function()
                     ret = 1;
                 break;
 
-            case 1: // User
-                if(a.user < b.user)
+            case 1: // Command
+                if(a.command.toLowerCase()  < b.command.toLowerCase() )
                     ret = -1;
-                if(a.user > b.user)
+                if(a.command.toLowerCase()  > b.command.toLowerCase() )
                     ret = 1;
                 break;
 
-            case 2: // CPU
+            case 2: // User
+                if(a.user.toLowerCase() < b.user.toLowerCase() )
+                    ret = -1;
+                if(a.user.toLowerCase()  > b.user.toLowerCase() )
+                    ret = 1;
+                break;
+
+            case 3: // CPU
                 if(parseFloat(a.cpu) < parseFloat(b.cpu))
                     ret = -1;
                 if(parseFloat(a.cpu) > parseFloat(b.cpu))
                     ret = 1;
                 break;
-            case 3: // RAM
+            case 4: // RAM
                 if(parseFloat(a.ram) < parseFloat(b.ram))
                     ret = -1;
                 if(parseFloat(a.ram) > parseFloat(b.ram))
                     ret = 1;
                 break;
 
-            case 4: // VIR
+            case 5: // VIR
                 if(parseFloat(a.vir) < parseFloat(b.vir))
                     ret = -1;
                 if(parseFloat(a.vir) > parseFloat(b.vir))
                     ret = 1;
                 break;
 
-            case 5: // Time
+            case 6: // Time
                 if(a.time < b.time)
                     ret = -1;
                 if(a.time > b.time)
                     ret = 1;
                 break;
 
-            case 6: // Command
-                if(a.command < b.command)
-                    ret = -1;
-                if(a.command > b.command)
-                    ret = 1;
-                break;
+
         }
 
         if (self.reverse) {
@@ -206,25 +224,116 @@ ProcessTable.prototype.renderProcList = function()
     this.list.html("")
     var row = []
     var i = 0;
+
+    this.resetTotal()
+
     this.rpi.processes.sort(compare)
     for (proc in this.rpi.processes) {
+        if (!this.matchSearch(this.rpi.processes[proc]))
+        {
+            continue;
+        }
+
         row = [];
         i = 0;
 
         row[i++] = "<tr>";
         row[i++] = "<td>" +     this.rpi.processes[proc].pid    + "</td>";
+        row[i++] = "<td><strong>" +     this.rpi.processes[proc].command + "</strong></td>";
         row[i++] = "<td>" +     this.rpi.processes[proc].user   + "</td>";
         row[i++] = "<td><em>" + this.rpi.processes[proc].cpu    + "</em></td>";
         row[i++] = "<td><em>" + this.rpi.processes[proc].mem    + "</em></td>";
         row[i++] = "<td>" +     this.rpi.processes[proc].vir    + "</td>";
         row[i++] = "<td>" +     this.rpi.processes[proc].time   + "</td>";
-        row[i++] = "<td>" +     this.rpi.processes[proc].command + "</td>";
+        row[i++] = "</tr>";
+
+        this.list.append(row.join(""))
+        this.addToTotal(this.rpi.processes[proc])
+    }
+
+    if (this.list.html() == "") {
+        this.list.html("<h2>No results</h2>")
+    } else {
+        // append total
+        row = [];
+        i = 0;
+
+        row[i++] = "<tr>";
+        row[i++] = "<td><strong>Total</strong></td>";
+        row[i++] = "<td>-</td>";
+        row[i++] = "<td>-</td>";
+        row[i++] = "<td><em>"+ this.total.cpu.toFixed(1) +"</em></td>";
+        row[i++] = "<td><em>"+ this.total.mem.toFixed(1) +"</em></td>";
+        row[i++] = "<td><em>"+ this.total.vir.toFixed(1) +"</em></td>";
+        row[i++] = "<td> "+ this.total.time +" </td>";
         row[i++] = "</tr>";
 
         this.list.append(row.join(""))
     }
 }
 
+ProcessTable.prototype.addToTotal = function(obj) {
+    this.total.cpu += parseFloat(obj.cpu);
+    this.total.mem += parseFloat(obj.mem);
+    this.total.vir += parseFloat(obj.vir);
+    this.addTime(obj.time);
+}
+
+ProcessTable.prototype.addTime = function(time){
+    var carry;
+
+    var oldTime = this.total.time.split(":"); // ["0", "00.00"]
+    var addTime = time.split(":");
+
+    var oldSecMil = oldTime[1].split("."); // ["00","00"]
+    var addSecMil = addTime[1].split(".");
+
+    var milliseconds = parseInt(oldSecMil[1]) + parseInt(addSecMil[1]);
+    var newMilliseconds = milliseconds % 100;
+    carry = (milliseconds - newMilliseconds)/100;
+
+    var seconds = parseInt(oldSecMil[0]) + parseInt(addSecMil[0]) + carry;
+    var newSeconds = seconds % 60;
+    carry = (seconds - newSeconds) / 60;
+
+    var minutes = parseInt(oldTime[0]) + parseInt(addTime[0]) + carry;
+
+    if (newMilliseconds < 10) {
+        newMilliseconds = "0" + newMilliseconds;
+    }
+
+    if (newSeconds < 10) {
+        newSeconds = "0" + newSeconds;
+    }
+
+    this.total.time = minutes + ":" + newSeconds + "." + newMilliseconds;
+
+}
+
+ProcessTable.prototype.resetTotal = function() {
+    this.total = {
+        cpu: 0.0,
+        mem: 0.0,
+        vir: 0.0,
+        time: "0:00.00"
+    };
+}
+
+// match each prop of object with search text
+ProcessTable.prototype.matchSearch = function(object)
+{
+    var text = this.searchBox.val();
+    var match = false;
+
+    for (el in object) {
+        if (object[el].match(text) != null) {
+            match = true
+            break;
+        }
+    }
+
+    return match
+}
 
 function HWComponent( id, rpi )
 {
