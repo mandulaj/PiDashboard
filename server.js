0b6bd3829e4ed8d83468bdc5ec79dbd7d84874f5
Jakub Mandula <jakub.aludnam@gmail.com>
2014-10-13 19:09:43 +0200

    disable Linux check for dev

--- server.js
+++ server.js
@@ -4,22 +4,22 @@ var http            = require("http"), // needed for express server
     https           = require("https"), // we want to be secure
     express         = require('express'), // we need to route the users around efficiently
     passport        = require('passport'), // used for authentication
-    cookieParser    = require('cookie-parser'),   
+    cookieParser    = require('cookie-parser'),
     session         = require('express-session'),
     bodyParser      = require('body-parser'),
 
     // Following are used for user authentication
-    fs              = require("fs"), 
+    fs              = require("fs"),
     os              = require("os"),
-    
+
     colors          = require("colors"), // make nice output
     optimist        = require("optimist").argv,
-    
+
     config          = require("./config/config.json"), // config file
-    
+
     port            = optimist.p || config.port, // port we will use
     exec            = require('child_process').exec,
-    
+
     PiDash          = require('./lib/PiDash.js'), // lib used for geting computer statistics
     flash           = require('connect-flash'),
     socketioJwt   = require("socketio-jwt");
@@ -30,24 +30,24 @@ var server;
 var socketio;
 setup();
 
-var passportConfig = require("./config/passport.js")(passport,config); 
+var passportConfig = require("./config/passport.js")(passport,config);
 
 
 // Serving static files
 app.use("/static", express.static(__dirname + "/public/static"));
 
 //app.use(logger())
-app.use(cookieParser()); 
+app.use(cookieParser());
 app.use(bodyParser()); // TODO: Didn't work with bodyParser.json()
-app.set('view engine', 'ejs');    
+app.set('view engine', 'ejs');
 
 app.use(session({ secret: config.cookieSecret })); // session secret
 app.use(passport.initialize());
 app.use(passport.session());
 
-function requireHTTPS(req, res, next) 
+function requireHTTPS(req, res, next)
 {
-    if (!req.secure) 
+    if (!req.secure)
     {
          return res.redirect('https://' + req.get('host') + req.url);
     }
@@ -57,22 +57,23 @@ function requireHTTPS(req, res, next)
 
 function setup() {
     'use strict';
-    
+
     // Check OS (We only work on raspberry)
     if(os.type() != "Linux")
     {
         console.error("You are not running Linux. Exiting ... \n".red);
-        process.exit(0);
+        // TODO: uncomment this at the end
+        //process.exit(0);
     }
-    
-    
+
+
     // Help display
     if (optimist.help || optimist.h)
     {
         require('./lib/help_log.js');
         process.exit(0);
     }
-    
+
     // Prepare keys and certs if we run https
     if (optimist.cert || optimist.key || config.forceSSL)
     {
@@ -84,7 +85,7 @@ function setup() {
         if (optimist.cert)  cert = optimist.cert; // If provided from cmd
         if (optimist.key)   key = optimist.key;
 
-        if (!cert.length)   cert = config.paths.cert; //else grab the default 
+        if (!cert.length)   cert = config.paths.cert; //else grab the default
         if (!key.length)    key = config.paths.key;
 
         if (!fs.existsSync(key))
@@ -100,7 +101,7 @@ function setup() {
             process.exit(1);
         }
     }
-    
+
     // bind to server
     if (config.forceSSL)
     {
@@ -129,7 +130,7 @@ function setup() {
 
         console.log("Server running at => ".green + "http" + console_sufix +"/localhost:" + port);
     });
-    
+
     socketio = require('socket.io')(server);
     socketio.use(socketioJwt.authorize({
         secret: config.socketIOSecret,
