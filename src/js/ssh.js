c973bab683cd2841fb712e29044274281d8a27e1
Jakub Mandula <jakub.aludnam@gmail.com>
2014-11-16 12:49:45 +0100

    add window tab methods

--- src/js/ssh.js
+++ src/js/ssh.js
@@ -1,74 +1,199 @@
-function Session(socket){
-  this.terms = [];
-  this.socket = socket;
-  this.termDest =  document.getElementById("ssh");
-}
+function Session(socket) {
+  var self = this;
 
-Session.prototype.newTerminal = function(cb){
-  var socket = this.socket,
-    self = this;
+  this.terms = []; // Array of all the terminal references
+  this.windows = []; // Array of all windows
+  this.socket = socket; // socket used to communicate with server
 
-  // Make a new terminal instance
-  var term = new Terminal({
-    cols: 80,
-    rows: 24,
-    useStyle: true,
-    screenKeys: true,
-    cursorBlink: true
-  });
 
-  // request the creation of a new terminal
-  socket.emit('createNew', {
-    cols: 80,
-    rows: 34
-  });
+  this.socket.on('confirm', function(data) {
+    var socket = self.socket;
 
-  // if we get a confirmation
-  socket.on('confirm', function(data) {
+    // Find the window that requested the creation of a new Terminal
+    var window = self.findWindow(data.windowID);
+    if (window === -1) {
+      socket.emit('kill', data.id);
+      return;
+    }
+    // Find the tab in the window that requested a new Terminal
+    var tab = window.findTab(data.tabID);
+    if (tab === -1) {
+      socket.emit('kill', data.id);
+      return;
+    }
+
+    // Make a new terminal instance
+    var term = new Terminal({
+      cols: 80,
+      rows: 24,
+      useStyle: true,
+      screenKeys: true,
+      cursorBlink: true
+    });
+
+    // Get the terminal id which the server provided
     var id = data.id;
+    tab.setTerminalId(id);
+    // Set up the terminal write
     term.on('data', function(data) {
       socket.emit('data', id, data);
     });
 
+    // Get the terminal title
     term.on('title', function(title) {
+      // tab.setTitle(title);
       console.log(title);
     });
 
-    term
+    // open the terminal in the tab
+    term.open(tab.container);
 
-    term.open(this.termDest);
-
-    socket.on('data', function(id, data) {
-      term.write(data);
-    });
-
-    socket.on('disconnect', function() {
-      term.destroy();
-      delete self.terms[id];
-    });
-
-
-    socket.on('kill', function(id){
-      if(!self.term[id]) {
-        self.term[id].destroy();
-        delete self.term[id];
-      }
-    });
 
     self.terms[id] = term; // add the term to the list of terms
 
     //socket.removeListener('confirm')
   });
+
+  // on data rout the it to the correct terminal using the id
+  socket.on('data', function(id, data) {
+    if (self.terms[id]) {
+      self.terms[id].write(data);
+    }
+  });
+
+  /* Disconnect all terms
+  socket.on('disconnect', function() {
+    term.destroy();
+    delete self.terms[id];
+  });*/
+
+  //TODO remove the tac/window on exit
+  socket.on('kill', function(id) {
+    if (self.terms[id]) {
+      var tab = self.findTerminalTab(id);
+
+      if (tab !== -1) {
+        self.terms[id].destroy();
+        delete self.terms[id];
+        tab.destroy();
+      }
+    }
+  });
+
 }
+
+// request a new terminal
+Session.prototype.newTerminal = function(windowID, tabID) {
+  var socket = this.socket;
+
+  // request the creation of a new terminal
+  socket.emit('createNew', {
+    cols: 80,
+    rows: 34,
+    windowID: windowID,
+    tabID: tabID
+  });
+
+};
+
+Session.prototype.createWindow = function() {
+  var window = new Window(this);
+  this.windows.push(window);
+};
+
+// Find a window in the session using its id
+Session.prototype.findWindow = function(id) {
+  for (var i in this.windows) {
+    if (this.windows[i].id === id) {
+      return this.windows[i];
+    }
+  }
+  return -1;
+};
+
+// Find a tab using the terminal id
+Session.prototype.findTerminalTab = function(id) {
+  for (var i in this.windows) {
+    for (var j in this.windows[i].tabs) {
+      if (this.windows[i].tabs[j].terminalID === id) {
+        return this.windows[i].tabs[j];
+      }
+    }
+  }
+  return -1;
+};
+
+function Window(session){
+  this.id = Math.floor(Math.random()*1e14); // random id
+  this.session = session;
+  this.tabs = []; // List of tabs in the window
+
+  $("#ssh").append("<div class='window'></div>"); // Create the window container
+  this.container = $("#ssh div:last-child")[0]; // select the container
+  $(this.container).append("<div class='bar'></div>");
+  $(this.container).draggable({
+    handle: ".bar",
+    containment: "parent"
+  });
+  // Create first tab
+  this.createTab();
+}
+
+// Create a new tab in the window
+Window.prototype.createTab = function(){
+  var tab = new Tab(this);
+  this.tabs.push(tab);
+  this.session.newTerminal(this.id, tab.id);
+};
+
+// Find the tab in window using its id
+Window.prototype.findTab = function(id) {
+  for(var i in this.tabs) {
+    if (this.tabs[i].id === id) {
+      return this.tabs[i];
+    }
+  }
+  return -1;
+};
+
+Window.prototype.destroy = function(){
+  $(this.container).remove();
+};
+
+function Tab(window) {
+  this.id = Math.floor(Math.random()*1e14);
+  this.terminalID = null;
+  this.window = window;
+  $(this.window.container).append("<div class='tab'></div>");
+  this.container = $(this.window.container).children().last()[0];
+}
+
+Tab.prototype.setTerminalId = function(id){
+  this.terminalID = id;
+};
+
+Tab.prototype.destroy = function(){
+  if (this.window.tabs.length <= 1) {
+    this.window.destroy();
+  }
+  $(this.container).remove();
+};
+
 
 
 $(document).ready(function() {
   var socket = io("/ssh", {
     'query': 'token=' + sessionStorage.getItem("socketIOtoken")
   });
+  var session = new Session(socket);
 
   socket.on('connect', function() {
-  }
-
+    session.createWindow();
+    session.createWindow();
+    /*
+    session.newTerminal();
+    session.newTerminal(document.getElementById("ssh2"));
+    session.newTerminal(document.getElementById("ssh3"));
+  */
   });
 });
